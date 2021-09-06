50e09571644a522f89794634da7e88d4278e19ac
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2021-09-06 11:59:07 +0200

    feat: add basic service worker

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+    console.log('Instalasi service worker sukses:', registration)
+  }, function (error) {
+    console.log('Instalasi service worker gagal:', error)
+  })
+} else {
+  console.log('Service worker tidak didukung.')
+}
+
 function getComponent(root) {
   const url = new URL(window.location.href)
 
---
+++ public/service-worker.js
@@ -0,0 +1,85 @@
+/*
+* Script service worker paling basic
+* referensi: https://googlechrome.github.io/samples/service-worker/basic/
+*/
+
+// Kalau ada update di service worker, ganti nama cache-nya
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// List url yang mau di precache
+const PRECACHE_URLS = [
+  // HTML
+  'index.html',
+  './', // alias untuk index.html
+  '404.html',
+  '404/index.html',
+  'bookmark/index.html',
+  'detail/index.html',
+  // style
+  'style.css',
+  // js
+  'js/utils.js',
+  'js/api.js',
+  'js/components/NavBar.js',
+  'js/pages/BookmarkPage.js',
+  'js/pages/DetailPage.js',
+  'js/pages/HomePage.js',
+  'js/app.js',
+  // images
+  'images/arrow-left.svg',
+  'images/bookmark-blue.svg',
+  'images/bookmark-white.svg',
+  'images/bookmark.svg',
+  'images/circle-loading-black.svg',
+  'images/home-blue.svg',
+  'images/home.svg',
+  'images/logo192.png',
+  'images/logo512.png',
+  'images/refresh.svg',
+];
+
+// Di trigger waktu PWA pertama kali diinstal
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// Handler activate untuk cleanup caches
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch di intercept biar bisa ngambil dari cache dulu
+self.addEventListener('fetch', event => {
+  // Skip request cross-origin
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
